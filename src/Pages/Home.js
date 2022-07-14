b2051a21b8ca0ce183516d5c4b7f11bb27a220b9
Grant Alexander <grantalexander@argead.mynetworksettings.com>
2022-07-14 13:09:51 -0400

    updated proxy to get server talking

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from "react";
 
 function Home() {
 
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+      fetch("/api")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+    }, []);
     
-    
-    return <div>This is the home page
-        </div>;
+    return <div>This is the home page <p> test {data}</p></div>;
     
 
 }
