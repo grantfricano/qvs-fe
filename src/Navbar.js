d6292cdf594dcca47303d6a64497fe943576f11b
Grant Alexander <grant.fricano@gmail.com>
2022-07-17 12:03:26 -0400

    updated css styling for name

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 export default function Navbar() {
     return (
     <nav className="nav">  
-      <Link to="/">Home</Link>
+      <Link to="/"><div className="navbar-name">Grant</div>Fricano</Link>
       <ul>
         <li><Link to="/submitdata">Submit Form</Link></li>
         <li><Link to="/search">Search</Link></li>
