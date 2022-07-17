35632f48cd2af92b2022a9e2653b250ead2fe915
Grant Alexander <grant.fricano@gmail.com>
2022-07-17 12:29:50 -0400

    more css testing

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 export default function Navbar() {
     return (
     <nav className="nav">  
-      <Link to="/"><div className="navbar-name">Grant</div>Fricano</Link>
+      <Link to="/"><span className="navbar-name">Grant </span><span className="navbar-lastname">&nbsp;Fricano</span></Link>
       <ul className="navbar-links"> 
         <li><Link to="/submitdata">Submit Form</Link></li>
         <li><Link to="/search">Search</Link></li>
