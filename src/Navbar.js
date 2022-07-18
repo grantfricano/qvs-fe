a109f3ef3dc11f6b03b14f8ba49098ff9b69080b
Grant Alexander <grant.fricano@gmail.com>
2022-07-18 14:39:06 -0400

    more navbar updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,14 +9,10 @@ export default function Navbar() {
     <nav className="nav">  
       <Container>
         <Row>
-          <Col ><Link to="/"><div className="navbar-name">Grant</div><div className="navbar-lastname">Fricano</div></Link></Col>
-          <Col></Col>
-          <Col></Col>
-          <Col></Col>
-          
-          <Col><div className="navbar-links"> <Link to="/submitdata">Contact</Link></div></Col>
-          <Col><div className="navbar-links"> <Link to="/search">Search</Link></div></Col>
-          <Col><div className="navbar-links"> <Link to="/mywork">My Work </Link></div></Col>
+          <Col md = {6} lg = {8} sm = {12}><Link to="/"><div className="navbar-name">Grant</div><div className="navbar-lastname">Fricano</div></Link></Col>
+          <Col md = {2} lg = {1} sm = {12}><div className="navbar-links"> <Link to="/submitdata">Contact</Link></div></Col>
+          <Col md = {2} lg = {1} sm = {12}><div className="navbar-links"> <Link to="/search">Search</Link></div></Col>
+          <Col md = {2} lg = {1} sm = {12}><div className="navbar-links"> <Link to="/mywork">My Work </Link></div></Col>
         </Row>
       </Container>
 
